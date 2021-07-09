37f867ee148fb908947b36dca22a08cd166b50ca
Animesh Goyal <animesh.goyal@tieto.com>
2021-07-09 17:50:58 +0530

    Fixing compilation issue

--- file-handler/src/main/java/com/file/handler/message/producer/FileHandlerMessageCreator.java
+++ file-handler/src/main/java/com/file/handler/message/producer/FileHandlerMessageCreator.java
@@ -8,12 +8,12 @@ import javax.jms.Session;
 import javax.jms.TextMessage;
 
 public class FileHandlerMessageCreator implements MessageCreator {
-  private final String message;
-  public FileHandlerMessageCreator(String message) {
-    this.message = message;
+  private final String textMessage;
+  public FileHandlerMessageCreator(String textMessage) {
+    this.textMessage = textMessage;
   }
   public Message createMessage(Session session) throws JMSException {
-    TextMessage message = session.createTextMessage(message);
+    TextMessage message = session.createTextMessage(textMessage);
     return message;
   }
 }
